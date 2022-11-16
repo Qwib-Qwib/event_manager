986eec3189874fa17054b368cc13c41399363263
Qwib-Qwib <jp93rom@orange.fr>
2022-11-16 20:23:19 +0100

    Add code to clean up, normalize and print phone numbers. Delete useless assignements to variables.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,23 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(number)
+  number = keep_only_digits(number)
+  if number.length < 10 || number.length > 11 || (number.length == 11 && number[0] != '1')
+    'Invalid phone number!'
+  elsif number.length == 11 && number[0] == '1'
+    number.reverse.chop.reverse
+  else
+    number
+  end
+end
+
+def keep_only_digits(number)
+  number.each_char do |char|
+    number.delete!(char) if (48..57).include?(char.codepoints[0]) == false
+  end
+end
+
 def print_officials_names(zipcode)
   officials = extract_officials_info(zipcode)
   officials_names = officials.each_with_object([]) do |official_info, officials_array|
@@ -47,11 +64,12 @@ end
 Dir.mkdir('letters') unless Dir.exist?('letters')
 content.each do |row|
   row[:zipcode] = clean_zipcode(row[:zipcode])
-  print "#{row[:first_name]} #{row[:zipcode]} "
+  row[:homephone] = clean_phone_number(row[:homephone])
+  print "#{row[:first_name]} #{row[:zipcode]} #{row[:homephone]} "
   begin
-    officials_names = print_officials_names(row[:zipcode])
+    print_officials_names(row[:zipcode])
   rescue
-    print officials_names = 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    print 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
   puts ''
   create_form_letter(row, extract_officials_info(row[:zipcode]))
